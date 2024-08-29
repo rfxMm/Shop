65e997678c91376a5f6b7d48ff21bd16d658c82f
rfxM <eggaby.eg@gmail.com>
2024-08-29 12:47:58 +0300

    Primul meu commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from "./Pages/Home"
+import Collection from "./Pages/Collection"
+import About from "./Pages/About"
+import Contact from "./Pages/Contact"
+import Product from "./Pages/Prodact"
+import Card from "./Pages/Cart"
+import Login from "./Pages/Login"
+import Order from "./Pages/Orders"
+import PlaceOrder from './Pages/PlaceOrder'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md: px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />}/>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/card' element={<Card />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place_order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Order />} /> 
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Prodact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Prodact = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Prodact
